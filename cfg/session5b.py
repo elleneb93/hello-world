9a12276a3d80fabb818d4577184c4dd3ac0abdf2
Ellen Buckley <34159577+elleneb93@users.noreply.github.com>
2018-03-08 19:03:37 +0000

    remove keys

--- cfg/session5b.py
+++ cfg/session5b.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = 'SaF7id87RCPFWQnWrCXfFKEgu'
-consumer_secret = 'CvNdmCM9FInwbX5TIo80E0f2FhU0geRxAxiLGH4B5aFdpjZmWg'
-access_token = '68732555-Vo24ky13lMmFwlLfL6F8z5dLLqSs6ba8EjRojWoU3'
-access_token_secret = 'zFK8WtJlkUoc6C8LiZScAbAXfHgkxiZaGKWjH0zCrti3l'
+consumer_key = 'xxx'
+consumer_secret = 'xxx'
+access_token = 'xxx'
+access_token_secret = 'xxx'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
