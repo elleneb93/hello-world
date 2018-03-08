43a35a8d89207b706e3dd52898e8b14743495f0c
Ellen Buckley <ellenebuckley93@gmail.com>
2018-03-08 18:41:09 +0000

    remove keys (duh)

--- cfg/twitter_general.py
+++ cfg/twitter_general.py
@@ -8,10 +8,10 @@ Modified by Darren on 3 Mar 2018.
 
 import tweepy
 
-consumer_key = 'SaF7id87RCPFWQnWrCXfFKEgu'
-consumer_secret = 'CvNdmCM9FInwbX5TIo80E0f2FhU0geRxAxiLGH4B5aFdpjZmWg'
-access_token = '68732555-Vo24ky13lMmFwlLfL6F8z5dLLqSs6ba8EjRojWoU3'
-access_token_sec = 'zFK8WtJlkUoc6C8LiZScAbAXfHgkxiZaGKWjH0zCrti3l'
+consumer_key = 'xxx'
+consumer_secret = 'xxx'
+access_token = 'xxx'
+access_token_sec = 'xxx'
 
 def authenticate():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
